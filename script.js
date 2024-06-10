0ab140d30a92ed70b4c0a128c1c6ea2264121ca3
souvikmaity797 <121762736+souvikmaity797@users.noreply.github.com>
2024-06-11 02:20:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const target =document.getElementById('target');
+let yes=document.getElementById('yes');
+yes.addEventListener('click',()=>{
+    alert("I Love you too babe!😘");
+})
+function moveTarget(){
+    const maxWidth=400;
+    const maxHeight=400;
+
+    const ranomX=Math.floor(Math.random() * maxWidth);
+    const ranomY=Math.floor(Math.random() * maxHeight);
+    target.style.left=ranomX+'px';
+    target.style.top=ranomY+'px';
+    
+}
+target.addEventListener('mouseenter',function(){
+    moveTarget();
+})
